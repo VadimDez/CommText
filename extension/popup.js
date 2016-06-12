47ff86ff685be15cca127508b457484622c3cc05
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-06-12 08:09:15 +0200

    group

--- extension/popup.js
+++ extension/popup.js
@@ -1,17 +1,42 @@
 document.addEventListener('DOMContentLoaded', function () {
   var settings = { access: 'public' };
+  var $groupElem = document.querySelector('#commtext-settings__group');
+  var $groupInputElem = document.querySelector('#commtext-settings__group input');
+  var $privateAccessElem = document.querySelector('#commtext-settings [name="access"][value="private"]');
+  var $nameElem = document.querySelector('#commtext-settings [name="pseudonym"]');
+
+  var fn = function () {
+    updateGroup(this.checked && this.value === 'private');
+  };
+
+  var updateGroup = function (display) {
+    if (!display) {
+      $groupInputElem.value = '';
+      $groupElem.style.display = 'none';
+      return;
+    }
+
+    $groupElem.style.display = 'block';
+  };
+
+  $privateAccessElem.addEventListener('change', fn);
+  document.querySelector('#commtext-settings [name="access"][value="public"]').addEventListener('change', fn);
 
   chrome.storage.local.get('settings', function (result) {
     settings = result.settings;
 
-    document.querySelector('#commtext-settings [name="pseudonym"]').value = settings.pseudonym;
+    $nameElem.value = settings.pseudonym;
     document.querySelector('#commtext-settings [name="access"][value="' + settings.access + '"]').checked = true;
+    $groupInputElem.value = settings.group;
+
+    updateGroup($privateAccessElem.checked);
   });
 
   document.querySelector('#commtext-settings__save').addEventListener('click', function () {
     settings = {
-      pseudonym: document.querySelector('#commtext-settings [name="pseudonym"]').value,
-      access: getSelectedAccess()
+      pseudonym: $nameElemvalue,
+      access: getSelectedAccess(),
+      group: $groupInputElem.value || null
     };
 
     chrome.storage.local.set({settings: settings});
@@ -32,6 +57,7 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function sendMessage(message) {
+    console.log(message);
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, message);
       }
